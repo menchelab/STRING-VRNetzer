3b5da2c5948027ea6e6e82403140f4ddee7bbd15
ObT1337 <a12048115@unet.univie.ac.at>
2022-07-18 12:39:04 +0200

    Used os.path.join instead /

--- src/settings.py
+++ src/settings.py
@@ -1,8 +1,9 @@
 import os
 
-_PROJECTS_PATH = os.path.abspath(f"{__file__}/../../static/projects")
-_NETWORKS_PATH = os.path.abspath(f"{__file__}/../../static/networks")
-_STYLES_PATH = os.path.abspath(f"{__file__}/../../static/styles")
+_WORKING_DIR = os.path.dirname(os.path.abspath(__file__))
+_PROJECTS_PATH = os.path.join(f"{_WORKING_DIR}", "..", "..", "static", "projects")
+_NETWORKS_PATH = os.path.join(f"{_WORKING_DIR}", "..", "..", "static", "networks")
+_STYLES_PATH = os.path.join(f"{_WORKING_DIR}", "..", "..", "static", "styles")
 os.makedirs(_PROJECTS_PATH, exist_ok=os.X_OK)
 os.makedirs(_NETWORKS_PATH, exist_ok=os.X_OK)
 os.makedirs(_STYLES_PATH, exist_ok=os.X_OK)
